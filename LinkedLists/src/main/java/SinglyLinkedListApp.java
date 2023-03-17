fc1b3928e298e03992e953f9251ec912a9f2a3ab
Hilal2013 <hilalozkul@gmail.com>
2023-03-17 18:24:02 -0400

    updated

--- LinkedLists/src/main/java/SinglyLinkedListApp.java
+++ LinkedLists/src/main/java/SinglyLinkedListApp.java
@@ -1,20 +1,27 @@
 public class SinglyLinkedListApp {
     public static void main(String[] args) {
-        MySinglyLinkedList mylist1=new MySinglyLinkedList();
-        System.out.println(mylist1.isEmpty());//true
+        MySinglyLinkedList mylist=new MySinglyLinkedList();
+        System.out.println(mylist.isEmpty());//true
+        for (int i = 0; i <10 ; i++) {
+            mylist.add(i);
+        }
+
+mylist.printNodes();//0=> 1=> 2=> 3=> 4=> 5=> 6=> 7=> 8=> 9=> null
+
+        mylist.deleteById(7);
+        mylist.printNodes();//0=> 1=> 2=> 3=> 4=> 5=> 6=> 8=> 9=> null// seven is gone
+        System.out.println("index of 1: "+mylist.indexOf(1));
+        mylist.addFirst(10);
+        mylist.printNodes();
+
+          MySinglyLinkedList mylist1=new MySinglyLinkedList();
         for (int i = 0; i <10 ; i++) {
             mylist1.add(i);
         }
+        mylist1.printNodes();//0=> 1=> 2=> 3=> 4=> 5=> 6=> 7=> 8=> 9=> null
 
-mylist1.printNodes();//0=> 1=> 2=> 3=> 4=> 5=> 6=> 7=> 8=> 9=> null
-
-        mylist1.deleteById(7);
-        mylist1.printNodes();//0=> 1=> 2=> 3=> 4=> 5=> 6=> 8=> 9=> null// seven is gone
-        System.out.println("index of 1: "+mylist1.indexOf(1));
-
-        //  MySinglyLinkedList mylist2=new MySinglyLinkedList();
-
-
-
+        System.out.println(mylist1.getKthFromLast(4));//5
+        mylist1.removeKthFromLast(4);
+        mylist1.printNodes();
     }
 }
