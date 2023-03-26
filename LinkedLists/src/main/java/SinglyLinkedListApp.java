f44341c9cc223e0d36ae8a22fb8a7dc3cac98722
Hilal2013 <hilalozkul@gmail.com>
2023-03-25 22:16:17 -0400

    updated

--- LinkedLists/src/main/java/SinglyLinkedListApp.java
+++ LinkedLists/src/main/java/SinglyLinkedListApp.java
@@ -7,12 +7,13 @@ public class SinglyLinkedListApp {
         }
 
 mylist.printNodes();//0=> 1=> 2=> 3=> 4=> 5=> 6=> 7=> 8=> 9=> null
-
-        mylist.deleteById(7);
+mylist.add(6);//0=> 1=> 2=> 3=> 4=> 5=> 6=> 7=> 8=> 9=> 6=> null
+        mylist.printNodes();
+        mylist.deleteById(6);
         mylist.printNodes();//0=> 1=> 2=> 3=> 4=> 5=> 6=> 8=> 9=> null// seven is gone
         System.out.println("index of 1: "+mylist.indexOf(1));//1
         mylist.addFirst(10);
-        mylist.printNodes();
+        mylist.printNodes();//10=> 0=> 1=> 2=> 3=> 4=> 5=> 6=> 8=> 9=> null
 
           MySinglyLinkedList mylist1=new MySinglyLinkedList();
         for (int i = 0; i <10 ; i++) {
@@ -23,5 +24,13 @@ mylist.printNodes();//0=> 1=> 2=> 3=> 4=> 5=> 6=> 7=> 8=> 9=> null
         System.out.println(mylist1.getKthFromLast(4));//6
         mylist1.removeKthFromLast(4);
         mylist1.printNodes();//0=> 1=> 2=> 3=> 4=> 5=> 7=> 8=> 9=> null
+mylist1.add(9);
+mylist.add(10);
+mylist1.add(10);
+        mylist1.printNodes();
+mylist1.removeDuplicates();
+        mylist1.printNodes();
+    //   mylist1.reverseList();
+    //    mylist1.printNodes();
     }
 }
