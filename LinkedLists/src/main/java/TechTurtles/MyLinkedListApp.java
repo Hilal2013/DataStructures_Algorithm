ad29bdd539e003638cbe0c5c970fd6b5b5dc3830
Hilal2013 <hilalozkul@gmail.com>
2023-03-27 18:35:32 -0400

    MyLinkedListApp

---
+++ LinkedLists/src/main/java/TechTurtles/MyLinkedListApp.java
@@ -0,0 +1,13 @@
+package TechTurtles;
+
+public class MyLinkedListApp {
+    public static void main(String[] args) {
+        MyLinkedList myList=new MyLinkedList();
+        for (int i = 1; i <13 ; i++) {
+            myList.addNodes(new Node(i));//1 => 2 => 3 => 4 => 5 => 6 => 7 => 8 => 9 => 10 => 11 => 12 => null
+        }
+      myList.printMyList();
+myList.deleteNNodesAfterMNodes(3,2);
+        myList.printMyList();
+    }
+}
