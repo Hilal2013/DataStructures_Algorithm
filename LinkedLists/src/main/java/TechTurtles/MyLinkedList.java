8e6e0e2f53989a8f074250bf207ad4870848eb82
Hilal2013 <hilalozkul@gmail.com>
2023-03-27 18:35:52 -0400

    MyLinkedList

---
+++ LinkedLists/src/main/java/TechTurtles/MyLinkedList.java
@@ -0,0 +1,70 @@
+package TechTurtles;
+
+public class MyLinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    public MyLinkedList() {
+        this.size = 0;
+    }
+
+    boolean isEmpty() {
+            return head == null;
+    }
+    void addNodes(Node node) {
+
+        if (isEmpty()){
+           head=tail=node;
+        }else{
+            tail.next=node;
+            tail=node;
+            size++;
+        }
+
+    }
+
+    void printMyList() {
+        Node current=head;
+
+while(current!=null){
+
+    System.out.print(current.data+" => ");
+    current=current.next;
+    if (current == null) System.out.print("null");
+}
+        System.out.println();
+    }
+
+
+    void deleteNNodesAfterMNodes(int n, int m) {
+        Node current=head;
+        Node prev=head;
+
+        while(current!=null){
+
+            for (int i = 1; i <= m; i++) {//m times current will go
+            prev=current;
+                current=current.next;
+        }
+
+            for (int i = 1; i <=n ; i++) {//n times removing
+                prev.next=current.next;
+                current.next=null;
+                size--;
+
+
+            }
+            prev=current;
+            current=current.next;
+            if (current == null) {return;}
+
+            }
+        }
+
+
+
+
+
+
+}
