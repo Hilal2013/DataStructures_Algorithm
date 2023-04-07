4d52ebc7d30c945a9fd71812897e6de10fe1c58e
Hilal2013 <hilalozkul@gmail.com>
2023-04-06 23:11:27 -0400

    RecursiveFibonacci

---
+++ Stacks/src/main/java/RecursiveFibonacci.java
@@ -0,0 +1,21 @@
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        System.out.println(fib(2));//1
+       System.out.println(fib(5));//5
+       System.out.println(fib(6));//8 0112358
+
+        System.out.println(fact(5));//120
+
+    }
+public static long fact(long n){
+        if(n<=1) return 1;
+        else
+            return n*fact(n-1);//call method again
+
+}
+    public static int fib(int n){
+        if (n==0) return 0;// termination
+        if (n==1) return 1;// termination
+        return fib(n-1)+fib(n-2);
+    }
+}
