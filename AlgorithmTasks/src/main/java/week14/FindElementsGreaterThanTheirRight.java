4644e44fbe69fd3145afe79e8154a5e2b1ffede1
Hilal2013 <hilalozkul@gmail.com>
2023-04-20 17:23:32 -0400

    updated

--- AlgorithmTasks/src/main/java/week14/FindElementsGreaterThanTheirRight.java
+++ AlgorithmTasks/src/main/java/week14/FindElementsGreaterThanTheirRight.java
@@ -3,21 +3,57 @@ package week14;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class FindElementsGreaterThanTheirRight {
     public static void main(String[] args) {
 
          int[] arr = {10, 4, 6, 3, 5};
-        // int[] arr = {3, 4, 6, 8, 5};
+       // int[] arr = {10, 4, 11, 3, 5};
+         //int[] arr = {3, 4, 6, 8, 5};
       //  int[] arr = {3};
         System.out.println(find(arr));
     }
 
     public static List<Integer> find(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
+
+        if (arr==null || arr.length==0) {
+          return null;
+        }
+        Deque<Integer> stack = new LinkedList<>();
+       //pop all the elements that are less than the current element
+        for (int value: arr) {
+            while (!stack.isEmpty() && stack.peekLast() < value) {
+
+                    stack.pollLast();
+                }
+            //push current element into the stack
+
+                stack.offerLast(value);
+
+            }
+
+          return (List<Integer>) stack;
+
+    }
+
+    @Test
+    public void testFind() {
+        Assertions.assertEquals(List.of(10,6,5),find(new int[]{10,4,6,3,5}));
+        Assertions.assertEquals(List.of(8,1),find(new int[]{3, 4, 6, 8, 1}));
+        Assertions.assertEquals(List.of(3),find(new int[]{3}));
+    }
+
+}
+/*
+Given an unsorted integer array, return all greater elements than all elements present to their right using
+Stack.
+Example:
+Input: [10, 4, 6, 3, 5]
+Output: [10, 6, 5]
+The elements that are greater than all elements to their right are 10, 6, and 5.
+10 > 4 & 10 > 6 & 10 > 3 & 10 > 5
+ Stack<Integer> stack = new Stack<>();
         int first = 0;
         int second = 1;
         if (arr.length == 1) {
@@ -39,23 +75,4 @@ public class FindElementsGreaterThanTheirRight {
         }
         stack.push(arr[arr.length - 1]);
         return new ArrayList<>(stack);
-    }
-
-    @Test
-    public void testFind() {
-        Assertions.assertEquals(List.of(10,6,5),find(new int[]{10,4,6,3,5}));
-        Assertions.assertEquals(List.of(8,1),find(new int[]{3, 4, 6, 8, 1}));
-        Assertions.assertEquals(List.of(3),find(new int[]{3}));
-    }
-
-}
-/*
-Given an unsorted integer array, return all greater elements than all elements present to their right using
-Stack.
-Example:
-Input: [10, 4, 6, 3, 5]
-Output: [10, 6, 5]
-The elements that are greater than all elements to their right are 10, 6, and 5.
-10 > 4 & 10 > 6 & 10 > 3 & 10 > 5
-
  */
