7c2631f565fb4dc96cfab5426780604c60d785ae
Hilal2013 <hilalozkul@gmail.com>
2023-04-20 23:21:31 -0400

    updated

--- AlgorithmProblems/src/main/java/reverseLinkedList/Solution.java
+++ AlgorithmProblems/src/main/java/reverseLinkedList/Solution.java
@@ -2,17 +2,29 @@ package reverseLinkedList;
 
 public class Solution {
     public static void main(String[] args) {
-        MySinglyLinkedList list=new MySinglyLinkedList();
+        MySinglyLinkedList list = new MySinglyLinkedList();
 
-        for(int i=0;i<10;i++) list.add(i);
-        list.printNodes();
+        for (int i = 0; i < 10; i++) list.add(i);
+        list.printNodes();//0=> 1=> 2=> 3=> 4=> 5=> 6=> 7=> 8=> 9=> null
         System.out.println("After reverse in one pass");
         reverse(list);
-        list.printNodes();
+        list.printNodes();//9=> 8=> 7=> 6=> 5=> 4=> 3=> 2=> 1=> 0=> null
     }
+
     static void reverse(MySinglyLinkedList list) {
-
-
+        Node prev = list.head;
+        Node current = list.head.next;
+        Node nextNode;
+        while (current != null) {
+            nextNode = current.next;
+            current.next = prev;//change the direction of current to prev
+            prev = current;//move prev to right to current
+            current = nextNode;//move current to right on the next node
+        }
+//adjust the tail and head
+        list.tail=list.head;
+        list.tail.next=null;
+        list.head=prev;
 
 
     }
