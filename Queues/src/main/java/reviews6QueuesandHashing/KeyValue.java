4e8f78a01699fa713af5c98a89218c46fc7ef91e
Hilal2013 <hilalozkul@gmail.com>
2023-04-30 14:01:09 -0400

    KeyValue

---
+++ Queues/src/main/java/reviews6QueuesandHashing/KeyValue.java
@@ -0,0 +1,11 @@
+package reviews6QueuesandHashing;
+
+public class KeyValue<U,V> {
+    U key;
+    V value;
+
+    public KeyValue(U key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
