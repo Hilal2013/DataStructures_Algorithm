24a001e9ea1ec38bc1d908f897f6d0789635f904
Hilal2013 <hilalozkul@gmail.com>
2023-05-01 22:57:28 -0400

    updated

--- Trees/src/main/java/MyTree.java
+++ Trees/src/main/java/MyTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyTree {
     TNode root;
 
@@ -18,22 +21,64 @@ public class MyTree {
             if (value <= current.value) {
                 //if left null insert there
                 if (current.leftChild == null) {
-              current.leftChild=newNode;
-              break;
+                    current.leftChild = newNode;
+                    break;
                 }//left not null branch left subtree!!!
-                current=current.leftChild;
+                current = current.leftChild;
             } else {
                 //if right null insert there
                 if (current.rightChild == null) {
-                    current.rightChild=newNode;
+                    current.rightChild = newNode;
                     break;
                 }//right not null branch right subtree!!!
-                current=current.rightChild;
+                current = current.rightChild;
             }
         }
 
-
     }
 
+    //preorder root-left-right
+    void preOrderTraversal(TNode root) {
+
+        if(root==null) return;//termination
+        System.out.print(root.value+", "); //visit root
+        preOrderTraversal(root.leftChild);//visit left subtree
+        preOrderTraversal(root.rightChild);//visit right subtree
+
+    }
+    //in-order left root-right
+    void inOrderTraversal(TNode root) {
+
+        if(root==null) return;//termination
+        inOrderTraversal(root.leftChild);//branch left subtree
+        System.out.print(root.value+", "); //visit root
+        inOrderTraversal(root.rightChild);//branch right subtree
+
+    }
+    //in-order left root-right
+    void postOrderTraversal(TNode root){
+        if (root==null) return;// termination
+        postOrderTraversal(root.leftChild); // branch and finish left subtree
+        postOrderTraversal(root.rightChild); // branch and finish right subtree
+        System.out.print(root.value+", ");  // visit root
+    }
+void levelOrderTraversal(TNode root){
+        //this isnot recurrisive
+    if (root==null) return;
+    Queue<TNode> queue=new LinkedList<>();
+    queue.add(root);
+    while(!queue.isEmpty()){
+        TNode toVisit=queue.poll();
+        System.out.print(toVisit.value+", ");
+        //im goona put visit' children
+        if(toVisit.leftChild!=null){
+            queue.add(toVisit.leftChild);
+        }
+        if(toVisit.rightChild!=null){
+            queue.add(toVisit.rightChild);
+        }
+    }
+
+}
 
 }
