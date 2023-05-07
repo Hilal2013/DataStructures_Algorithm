2f3fa946066ca152aa2710a86be1a619c13d2781
Hilal2013 <hilalozkul@gmail.com>
2023-05-07 13:25:54 -0400

    updated

--- Trees/src/main/java/TreeApp.java
+++ Trees/src/main/java/TreeApp.java
@@ -9,24 +9,29 @@ public class TreeApp {
 
         VisualizeTree.printTree(tree.root,null,false);
 
-     tree.preOrderTraversal(tree.root);//10, 6, 4, 5, 8, 9, 20, 17, 42, 29, 47,
-      System.out.println();
-       System.out.println("In Order Traversal :");
-       tree.inOrderTraversal(tree.root);//4, 5, 6, 8, 9, 10, 17, 20, 29, 42, 47,
-      System.out.println();
-      System.out.println("Post Order Traversal: ");
-      tree.postOrderTraversal(tree.root);//5, 4, 9, 8, 6, 17, 29, 47, 42, 20, 10,?????
-       System.out.println();
-        System.out.println("Level Order Traversal: ");
-       tree.levelOrderTraversal(tree.root);//10, 6, 20, 4, 8, 17, 42, 5, 9, 29, 47,
+//     tree.preOrderTraversal(tree.root);//10, 6, 4, 5, 8, 9, 20, 17, 42, 29, 47,
+//      System.out.println();
+//       System.out.println("In Order Traversal :");
+//       tree.inOrderTraversal(tree.root);//4, 5, 6, 8, 9, 10, 17, 20, 29, 42, 47,
+//      System.out.println();
+//      System.out.println("Post Order Traversal: ");
+//      tree.postOrderTraversal(tree.root);//5, 4, 9, 8, 6, 17, 29, 47, 42, 20, 10,?????
+//       System.out.println();
+//        System.out.println("Level Order Traversal: ");
+//       tree.levelOrderTraversal(tree.root);//10, 6, 20, 4, 8, 17, 42, 5, 9, 29, 47,
+        //   Task 1 : Contains Method
+        System.out.println("Tree contains 1?:  " + tree.contains(42));
+        // Task 2; isLeaf() method
+        System.out.println("Is root leaf? :   " + tree.isLeaf(tree.root));
+        tree.printLeaves(tree.root);//5, 9, 17, 29, 47,
+        System.out.println();
+        System.out.println("Height of the tree is: "+ tree.height(tree.root));//3
 
-//        tree.printLeaves(tree.root);
-//        System.out.println();
-//        System.out.println("Number of leaves:  " +tree.countLeaves(tree.root));
-//        System.out.println("Sum of Leaf Nodes: " + tree.findSumOfLeaves(tree.root));
-//        System.out.println("Height of the tree is: "+ tree.height(tree.root));
+      System.out.println("Number of leaves:  " +tree.countLeaves(tree.root));//5
+       System.out.println("Sum of Leaf Nodes: " + tree.findSumOfLeaves(tree.root));//107
+
 //       // System.out.println("Sum of Node Depths is: " + tree.calculateNodeDepthSums());
-//        System.out.println("Sum of all node values is: " + tree.calculateNodeSums());
+      System.out.println("Sum of all node values is: " + tree.calculateNodeSums(tree.root));//197
 
     }
 }
