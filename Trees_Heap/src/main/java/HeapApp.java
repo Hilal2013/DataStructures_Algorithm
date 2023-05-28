4ba0968a23e6b7f3b620949c045596a436f9cf98
Hilal2013 <hilalozkul@gmail.com>
2023-05-27 20:55:24 -0400

    HeapApp

---
+++ Trees_Heap/src/main/java/HeapApp.java
@@ -0,0 +1,19 @@
+public class HeapApp {
+    public static void main(String[] args) {
+        MyHeap heap=new MyHeap(10);
+        heap.insert(61);
+        heap.insert(120);
+        heap.insert(52);
+        heap.insert(45);
+        heap.insert(81);
+        heap.printHeap();//120, 81, 52, 45, 61,//max 120
+        heap.remove();
+        heap.printHeap();//81, 61, 52, 45,
+        heap.insert(130);
+        heap.printHeap();//130, 81, 52, 45, 61,
+        heap.remove();
+        heap.printHeap();//81, 61, 52, 45,
+        int[] numbers={1,2,5,6,8,21,0};
+
+    }
+}
