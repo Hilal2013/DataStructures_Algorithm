5eda141e62bf630ed640d9a26b665f3d1a5b131f
Hilal2013 <hilalozkul@gmail.com>
2023-05-31 20:39:47 -0400

    updated

--- AlgorithmTasks/src/main/java/week20/CalculateDistanceReadFile.java
+++ AlgorithmTasks/src/main/java/week20/CalculateDistanceReadFile.java
@@ -32,6 +32,22 @@ public class CalculateDistanceReadFile {
     }
 }
 /*
+You have (x, y) coordinates for 2 points and need to find the distance between them.
+Input:
+Your program should read lines from the file. Each line contains two coordinate pairs which are space-delimited.
+All x and y values are integers between -100 and 100.
+Output:
+Return the list of distance between the points. You do not need to round the results you receive.
+Test 1
+ (25, 4) (1, -6)
+Expected Output 26.0
+Test 2
+ (47, 43) (-25, -11)
+Expected Output 90.0
+. The formula for distance between two point (x1, y1) and (x2, y2) is
+Distance = $\sqrt{(x2-x1)^{2} + (y2-y1)^{2}}$
+ */
+/*
  for (String line : lines) {
             String[] each = line.split(" ");
             int x1 = Integer.parseInt(lines[0]);
