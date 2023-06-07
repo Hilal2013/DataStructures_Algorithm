e8cb49b455368c6a924e783991aa02be1069c4ee
Hilal2013 <hilalozkul@gmail.com>
2023-06-07 19:12:55 -0400

    JumpSearch

---
+++ SearchAlgorithm/src/main/java/JumpSearch.java
@@ -0,0 +1,16 @@
+public class JumpSearch {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = i;
+        }
+        System.out.println(jumpSearch(numbers, 6));
+
+    }
+    public static int jumpSearch(int[] array, int data){
+
+
+        return -1;
+    }
+
+}
