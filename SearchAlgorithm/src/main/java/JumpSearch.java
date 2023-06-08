912b82b992e1538ee64336ff017da815f4680c57
Hilal2013 <hilalozkul@gmail.com>
2023-06-08 09:29:04 -0400

    updated

--- SearchAlgorithm/src/main/java/JumpSearch.java
+++ SearchAlgorithm/src/main/java/JumpSearch.java
@@ -8,8 +8,17 @@ public class JumpSearch {
 
     }
     public static int jumpSearch(int[] array, int data){
-
-
+        int blockSize=(int)Math.sqrt(array.length);
+        int start=0;
+        int next=blockSize;
+        while (start< array.length && array[next-1]<data){//what is the last element=>next-1
+            start=next;
+            next=next+blockSize;
+            if (next> array.length) next= array.length;
+        }
+        for (int i = start; i <next ; i++) {
+            if (array[i]==data) return i;
+        }
         return -1;
     }
 
