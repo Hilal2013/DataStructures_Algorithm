a67f16efc24ec093c388c6c32553988303e38170
Hilal2013 <hilalozkul@gmail.com>
2023-06-17 19:18:26 -0400

    SortingMain

---
+++ SortingAlgorithm/src/main/java/SortingMain.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class SortingMain {
+    public static void main(String[] args) {
+        int[] numbers= {7,2,4,6,1,9,-1};
+     //   int[] numbers= {2,0,2,1,1,0};
+
+       System.out.println(Arrays.toString(InsertionSort.insertionSort(numbers)));
+       // System.out.println(Arrays.toString(BubbleSort.bubbleSort(numbers)));
+      //  System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));
+       // JD1MergeSort.mergeSort(numbers);
+     //     JD2QuickSort.qSort(numbers, 0, numbers.length-1);
+       //   System.out.println(Arrays.toString(numbers));
+
+
+    }
+}
